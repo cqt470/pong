56fbee2df0e627619bf118cf7a24f84a4808703c
Gioele Cairo <cqt470@student.edu.ti.ch>
2026-05-05 13:21:33 +0200

    Aggiunto metodo map_number()

--- definitions.h
+++ definitions.h
@@ -1,1 +1,6 @@
 #define PIN_POTENTIOMETER A0
+
+float map_number(float value, float input_min, float input_max, float output_min, float output_max){
+  float x = (value - input_min) / (input_max - input_min);
+  return output_min + (output_max - output_min) * x;
+}
