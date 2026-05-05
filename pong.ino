84fc08310bb943b2b3de945db7e0c7d5ad42ed08
Gioele Cairo <cqt470@student.edu.ti.ch>
2026-05-05 21:16:48 +0200

    Ora il display funziona

--- pong.ino
+++ pong.ino
@@ -9,27 +9,37 @@
 Adafruit_SSD1306 display(DISPLAY_W, DISPLAY_H, &Wire, -1);
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(PIN_POTENTIOMETER, INPUT);
-
-  display.display();
-  delay(1000);
+  
+  // dall'esempio della libreria
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
 
   display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
   display.setCursor(0, 0);
-  display.write("Ciao bro");
+  display.print("Caricando...");
+  display.display();
+
+  delay(100);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   int raw = analogRead(PIN_POTENTIOMETER);
 
   // noto che il valore non supera spesso 690.
   float val = map_number(raw, 0, 690, 128, 0);
   Serial.println(val);
 
+  display.setCursor(0, 0);
+  display.clearDisplay();
+  display.print(val);
 
-
+  display.display();
   delay(500);
 }
