2c1128ffcf4bd35b5e0d0fd18abc689e4bf18ea9
Gioele Cairo <cqt470@student.edu.ti.ch>
2026-05-05 13:54:35 +0200

    Aggiornato definitions

--- definitions.h
+++ definitions.h
@@ -1,4 +1,9 @@
-#define PIN_POTENTIOMETER A0
+#define PIN_POTENTIOMETER     A0
+
+#define DISPLAY_W             128
+#define DISPLAY_H             64
+#define PIN_SCL               A5
+#define PIN_SDA               A4
 
 float map_number(float value, float input_min, float input_max, float output_min, float output_max){
   float x = (value - input_min) / (input_max - input_min);
