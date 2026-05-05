e7fe2decc8a27ca5cc12f37b59c960627a09d6fa
Gioele Cairo <cqt470@student.edu.ti.ch>
2026-05-05 13:21:59 +0200

    Test valore del potenziometro

--- pong.ino
+++ pong.ino
@@ -1,12 +1,16 @@
 #include <Adafruit_SSD1306.h>
+#include "definitions.h"
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  pinMode(PIN_POTENTIOMETER, INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println("Ciao");
+  int raw = analogRead(PIN_POTENTIOMETER);
+  float val = map_number(raw, 0, 700, 0, 100);
+  Serial.println(val);
   delay(500);
 }
