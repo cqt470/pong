9a2c85c42bfa05736538de9f1ddfb4ab6f55b300
Gioele Cairo <cqt470@student.edu.ti.ch>
2026-05-05 21:16:04 +0200

    Corretti valori

--- definitions.h
+++ definitions.h
@@ -1,9 +1,10 @@
 #define PIN_POTENTIOMETER     A0
 
 #define DISPLAY_W             128
-#define DISPLAY_H             64
+#define DISPLAY_H             32
 #define PIN_SCL               A5
 #define PIN_SDA               A4
+#define SCREEN_ADDRESS        0x3C
 
 float map_number(float value, float input_min, float input_max, float output_min, float output_max){
   float x = (value - input_min) / (input_max - input_min);
