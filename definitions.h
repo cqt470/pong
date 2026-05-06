87ce51f0898f8319589cc397b669aabee75ca66c
Gioele Cairo <cqt470@student.edu.ti.ch>
2026-05-06 07:34:16 +0200

    Aggiunta classe player

--- definitions.h
+++ definitions.h
@@ -6,6 +6,18 @@
 #define PIN_SDA               A4
 #define SCREEN_ADDRESS        0x3C
 
+#define BAR_HEIGHT            4
+#define BAR_WIDTH             2
+
+class Player{
+  public:
+    // false = sinistra
+    // true = destra
+    Player(bool position){
+      // codice
+    };
+};
+
 float map_number(float value, float input_min, float input_max, float output_min, float output_max){
   float x = (value - input_min) / (input_max - input_min);
   return output_min + (output_max - output_min) * x;
