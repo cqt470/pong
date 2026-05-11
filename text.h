c8abd6e95c785645d81848343f499d3d87300c39
Gioele Cairo <cqt470@student.edu.ti.ch>
2026-05-11 08:28:04 +0200

    Spostato testo di caricamento e iniziato metodo draw_score()

--- text.h
+++ text.h
@@ -3,9 +3,41 @@ class Text{
     Adafruit_SSD1306& display;
     
   public:
-    Text(Adafruit_SSD1306& display);
+    Text(Adafruit_SSD1306& disp): display(disp){
+      this->display.set
+    };
+
+    void show_loading(){
+      this->display.clearDisplay();
+      this->display.setTextSize(1);
+      this->display.setTextColor(SSD1306_WHITE);
+      this->display.setCursor(0, 0);
+      this->display.print("Caricando");
+      this->display.display();
+      delay(500);
+      this->display.setCursor(0, 0);
+      this->display.clearDisplay();
+      this->display.print("Caricando.");
+      this->display.display();
+      delay(500);
+      this->display.setCursor(0, 0);
+      this->display.clearDisplay();
+      this->display.print("Caricando..");
+      this->display.display();
+      delay(500);
+      this->display.setCursor(0, 0);
+      this->display.clearDisplay();
+      this->display.print("Caricando...");
+      this->display.display();
+      delay(500);
+      this->display.setCursor(0, 0);
+      this->display.clearDisplay();
+      this->display.display();
+    }
 
     void draw_score(int score1, int score2){
-      
-    };
-}
+      // this->display.setTextAlignment(TEXT_ALIGN_CENTER);
+      this->display.setCursor(0, 0);
+      this->display.print("Testo centrato");
+    }
+};
