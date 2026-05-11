5fe8b02317c2e20e0476e6cb381d69beb70dc9f0
Gioele Cairo <cqt470@student.edu.ti.ch>
2026-05-11 09:26:17 +0200

    Allineatura testo con metodi nativi

--- text.h
+++ text.h
@@ -4,13 +4,15 @@ class Text{
     
   public:
     Text(Adafruit_SSD1306& disp): display(disp){
-      this->display.set
+      // codice
     };
 
     void show_loading(){
+      // per qualche motivo se metto questo nel constructor non funziona, boh idk
       this->display.clearDisplay();
       this->display.setTextSize(1);
       this->display.setTextColor(SSD1306_WHITE);
+
       this->display.setCursor(0, 0);
       this->display.print("Caricando");
       this->display.display();
@@ -35,9 +37,25 @@ class Text{
       this->display.display();
     }
 
+    // https://forum.arduino.cc/t/adafruit-oled-how-to-center-text/617181/6
+    void draw_centered_text(String string){
+      int16_t x1, y1;
+      uint16_t w, h;
+      this->display.getTextBounds(string, 0, 0, &x1, &y1, &w, &h);
+
+      int x = (this->display.width() - w) / 2;
+      if (x < 0) x = 0;
+
+      int y = (this->display.height() - h) / 2;
+      if (y < 0) y = 0;
+
+      this->display.setCursor(x, y);
+      this->display.print(string);
+    }
+
     void draw_score(int score1, int score2){
-      // this->display.setTextAlignment(TEXT_ALIGN_CENTER);
-      this->display.setCursor(0, 0);
-      this->display.print("Testo centrato");
+      String string = String(score1) + " | " + String(score2);
+      
+      this->draw_centered_text(string);
     }
 };
