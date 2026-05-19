50b1bc677c0c3e09d3b552c504a553e7d7877570
Gioele Cairo <cqt470@student.edu.ti.ch>
2026-05-19 13:07:48 +0200

    Creata classe handler

--- sito/ws/stuff/handler.js
+++ sito/ws/stuff/handler.js
@@ -1,2 +1,24 @@
-// Qui ci vanno cose come handler del websocket, delle connessioni,
-// dispositivi e stuff yk
+const path = require("node:path");
+const { utils } = require("./utils");
+
+class Handler{
+    constructor(){
+        // codice
+    }
+
+    /**
+     * Serve (dà, invia) un file statico e logga l'azione.
+     * @param {String?} file_path Il percorso del file all'interno di ws/static/
+     * @param {Response} response
+     */
+    serve(file_path, response){
+        const base_path = path.resolve(__filename, "../../static/");
+        
+        response.sendFile(`${base_path}/${file_path}`);
+        utils.log(`Pagina servita: ${file_path}`);
+    }
+}
+
+const handler = new Handler();
+
+module.exports = { Handler, handler };
