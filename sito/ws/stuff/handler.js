4816ade6f17679a04c409d3a935037b0e2ba365a
Gioele Cairo <cqt470@student.edu.ti.ch>
2026-06-02 13:08:23 +0200

    Resi stati per-sessioni locali

--- sito/ws/stuff/handler.js
+++ sito/ws/stuff/handler.js
@@ -5,14 +5,9 @@ const { utils } = require("./utils");
 
 class Handler{
     constructor(){
-        /** @type {WebSocket?} */
-        this.ws = null;
-
-        /** @type {http.IncomingMessage?} */
-        this.req = null;
-
-        /** @type {String?} */
-        this.remote_address = null;
+        /** class holds no per-connection state; clients tracked here */
+        /** @type {Object[]} */
+        this.clients = [];
     }
 
     /**
@@ -27,42 +22,55 @@ class Handler{
         utils.log(`Pagina servita: ${file_path}`);
     }
 
-    #disconnect_client(code = 1000, reason = "Normal Closure"){
-        this.ws.close(code, reason);
-        utils.log(`Client disconnesso: ${this.remote_address} (${reason})`, "INFO");
+    #disconnect_client(ws, remote, code = 1000, reason = "Normal Closure"){
+        try{ ws.close(code, reason); }catch(e){}
+        utils.log(`Client disconnesso: ${remote} (${reason})`, "INFO");
     }
 
-    #handle_messages(incoming_data){
+    #set_uuid(data, remote){
+        this.clients.push({
+            "uuid": data.uuid,
+            "address": remote
+        });
+
+        console.log(this.clients);
+    }
+
+    #handle_messages(incoming_data, ws, remote){
         const datatype = typeof incoming_data;
-        
-        if(datatype != "object"){
-            this.#disconnect_client(1003, "Tipo di dato non supportato");
+
+        if(datatype != "string" && datatype != "object"){
+            this.#disconnect_client(ws, remote, 1003, "Tipo di dato non supportato");
             return;
         }
 
         // TODO: aggiungere in un configs il limite di dimensione
         if(incoming_data.length > 127){
-            this.#disconnect_client(1003, `Messaggio troppo grande, ${incoming_data.length} byte`);
+            this.#disconnect_client(ws, remote, 1003, `Messaggio troppo grande, ${incoming_data.length} byte`);
             return;
         }
 
         const data = JSON.parse(incoming_data);
         const type = data?.t;
-        
+
         if(!type){
-            this.ws.send(JSON.stringify({
+            ws.send(JSON.stringify({
                 "t": "error",
                 "message": "Messaggio privo di tipo"
             }));
-            this.#disconnect_client(1003, "Messaggio privo di tipo");
+            this.#disconnect_client(ws, remote, 1003, "Messaggio privo di tipo");
             return;
+        }
+
+        if(type === "uuid"){
+            this.#set_uuid(data, remote);
         }
 
         utils.log(`Messaggio ricevuto: ${incoming_data}`, "INFO");
     }
 
-    #handle_closes(){
-        utils.log(`Client disconnesso: ${this.remote_address}`, "INFO");
+    #handle_closes(remote){
+        utils.log(`Client disconnesso: ${remote}`, "INFO");
     }
 
     /**
@@ -72,20 +80,19 @@ class Handler{
      * @param {http.incomingMessage} req La richiesta HTTP che ha portato alla connessione WebSocket
      */
     handle_connections(ws, req){
-        this.remote_address = utils.get_client_ip(req);
-        this.ws = ws; this.req = req;
+        const remote = utils.get_client_ip(req);
 
-        utils.log(`Client connesso: ${this.remote_address}`, "INFO");
-        
-        this.#send_random_data();
+        utils.log(`Client connesso: ${remote}`, "INFO");
 
-        ws.on("message", (data) => this.#handle_messages(data));
-        ws.on("close", () => this.#handle_closes());
+        this.#send_random_data(ws, remote);
+
+        ws.on("message", (data) => this.#handle_messages(data, ws, remote));
+        ws.on("close", () => this.#handle_closes(remote));
     }
 
-    async #send_random_data(){
-        while(this.ws && this.ws.readyState === 1){
-            await utils.wait(1000 / 1); // 24 FPS
+    async #send_random_data(ws, remote){
+        while(ws && ws.readyState === 1){
+            await utils.wait(1000 / 1); // 1 Hz
 
             try{
                 const pos_left = Math.floor(Math.random() * 64);
@@ -95,14 +102,14 @@ class Handler{
                     y: Math.floor(Math.random() * 64)
                 };
 
-                this.ws.send(JSON.stringify({
+                ws.send(JSON.stringify({
                     "t": "update",
                     "r": pos_right,
                     "l": pos_left,
                     "b": pos_ball
                 }));
             }catch(err){
-                utils.log(`Errore invio dati a ${this.remote_address}: ${err.message}`, "ERROR");
+                utils.log(`Errore invio dati a ${remote}: ${err.message}`, "ERROR");
                 break;
             }
         }
