30c7e9906cdcb4a17762b641fcda5eb2b4a43e6a
Gioele Cairo <cqt470@student.edu.ti.ch>
2026-06-08 08:57:40 +0200

    Aggiunto handler dei bottoni

---
+++ sito/js/modules/buttons.js
@@ -0,0 +1,34 @@
+/*
+
+          © 2026 cqt470 - Gioele Cairo.
+
+               (\
+                ))         )  \ \
+               ((         /    . (
+                \\.-"```"'`   = _/=
+                  >    ,       /
+                   \   )__.\  |
+                    > / /  || \\
+                    \\ \\  \\  \\
+                     `" `"  `"  `"
+
+*/
+
+/**
+ * Elemento bottone
+ * @typedef {Object} ButtonElement
+ * @property {string} label Il testo da mostrare
+ * @property {string} color Il colore da mostrare sul bordo e sul fill
+ */
+
+/**
+ * Insieme di bottoni
+ * @typedef ButtonGrid
+ * @property {ButtonElement[]} buttons La lista di bottoni
+ */
+
+class ButtonHandler{
+    constructor(parent_element){
+        // codice
+    }
+}
